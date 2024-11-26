46175344b6120e89db5c8e5b0791cda56671e7ae
Rogério M <rogerio.menezes2009@gmail.com>
2024-11-26 16:21:04 -0300

    alter table courses

--- src/main/resources/db/migration/V1__create-table-courses.sql
+++ src/main/resources/db/migration/V1__create-table-courses.sql
@@ -3,7 +3,7 @@ create table courses(
     name VARCHAR(100) not null unique,
     category VARCHAR(100) not null,
     shift VARCHAR(100) not null,
-    active BOOLEAN not null,
+    situation VARCHAR(50) NOT NULL,
     created_at TIMESTAMP not null ,
     updated_at TIMESTAMP not null
 );
