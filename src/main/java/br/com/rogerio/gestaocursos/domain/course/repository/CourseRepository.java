f1858299d4f8e570ef932609f293cd2acbfe683e
Rogério M <rogerio.menezes2009@gmail.com>
2024-11-26 16:24:58 -0300

    edit import

--- src/main/java/br/com/rogerio/gestaocursos/domain/course/repository/CourseRepository.java
+++ src/main/java/br/com/rogerio/gestaocursos/domain/course/repository/CourseRepository.java
@@ -1,5 +1,6 @@
-package br.com.rogerio.gestaocursos.course;
+package br.com.rogerio.gestaocursos.domain.course.repository;
 
+import br.com.rogerio.gestaocursos.domain.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
