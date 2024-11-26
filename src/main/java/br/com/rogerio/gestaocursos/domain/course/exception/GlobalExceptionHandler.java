5b53497298b6e462ccb78160c4809f1ac30ec169
Rogério M <rogerio.menezes2009@gmail.com>
2024-11-26 16:21:52 -0300

    creat Exceptions

--- src/main/java/br/com/rogerio/gestaocursos/domain/course/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/rogerio/gestaocursos/domain/course/exception/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
-package br.com.rogerio.gestaocursos.course.exception;
+package br.com.rogerio.gestaocursos.domain.course.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,4 +21,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleGenericException(Exception ex){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred");
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        if (ex.getMessage().contains("Invalid CourseStatus value")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Invalid value for enum 'situation'. Accepted values are: ACTIVE, PENDING, CLOSED, CANCELED.");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Malformed request: " + ex.getMessage());
+    }
+
 }
